f0cec87011a1516c837ba492e50a802e4feacd80
Serge Gernyak <sergeg1990@gmail.com>
2013-12-24 11:13:42 +0200

    Fixed engine

--- lib/jquery-raty-rails/engine.rb
+++ lib/jquery-raty-rails/engine.rb
@@ -1,7 +1,7 @@
 module JqueryRatyRails
   class Engine < ::Rails::Engine
     initializer 'jquery-raty-rails.load-jquery-raty-rails' do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+      app.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, "#{root}/vendor"
     end
   end
 end
